648537e87845f381d38ab02bd61f7f6eb3cb88ff
MNikov <68874299+MNikov@users.noreply.github.com>
2020-10-30 15:45:09 +0200

    Added all demos for Attributes and Methods

--- Attributes_and_Methods/non_zip_files/01E_photo_album.py
+++ Attributes_and_Methods/non_zip_files/01E_photo_album.py
@@ -1,39 +1,30 @@
 class PhotoAlbum:
-    def __init__(self, pages):
+    def __init__(self, pages: int):
         self.pages = pages
         self.photos = [[] for _ in range(self.pages)]
 
     @classmethod
     def from_photos_count(cls, photos_count: int):
-        cls(photos_count / 4)
+        return cls(photos_count // 4)
 
     def add_photo(self, label: str):
-        for i in range(self.pages):
-            if len(self.photos[i]) < 4:
-                self.photos[i].append(label)
-                return f"{label} photo added successfully on page {i + 1}" \
-                       f" slot {self.photos[i].index(label) + 1}"
+        for page_num, page in enumerate(self.photos):
+            if len(page) < 4:
+                page.append(label)
+                return f"{label} photo added successfully on page {page_num + 1}" \
+                       f" slot {page.index(label) + 1}"
         return "No more free spots"
 
     def display(self):
         result = ''
         dashes = f"{'-' * 11}\n"
-        result += dashes
         for page in self.photos:
-            to_add = ' '.join(['[]' for _ in range(len(page))])
-            result += to_add + '\n'
             result += dashes
-        return result[:-1]
-
-
-album = PhotoAlbum(2)
-
-print(album.add_photo("baby"))
-print(album.add_photo("first grade"))
-print(album.add_photo("eight grade"))
-print(album.add_photo("party with friends"))
-print(album.photos)
-print(album.add_photo("prom"))
-print(album.add_photo("wedding"))
-
-print(album.display())
+            for i in range(len(page)):
+                if i == len(page) - 1:
+                    result += '[]'
+                else:
+                    result += '[] '
+            result += '\n'
+        result += dashes
+        return result
