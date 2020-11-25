2c02ee85824580811106645a34849bddff7b22c3
MNikov <68874299+MNikov@users.noreply.github.com>
2020-11-25 10:21:13 +0200

    Implemented a properly working code for 05E(from Yavor)

--- Decorators/05E_cache.py
+++ Decorators/05E_cache.py
@@ -1,11 +1,25 @@
 def cache(func):
-    def wrapper(n):
-        value = func(n)
-        wrapper.log[n] = value
-        return value
+    log = {}
 
-    wrapper.log = {}
+    def wrapper(*args, **kwargs):
+        if args not in log:
+            res = func(*args, **kwargs)
+            log[args] = res
+            return res
+        return log[args]
+
+    wrapper.log = log
     return wrapper
+
+
+# def cache(func): NOT WORKING AS EXPECTED
+#     def wrapper(n):
+#         value = func(n)
+#         wrapper.log[n] = value
+#         return value
+#
+#     wrapper.log = {}
+#     return wrapper
 
 
 @cache
@@ -15,7 +29,7 @@ def fibonacci(n):
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 
-fibonacci(3)
+fibonacci(100)
 print(fibonacci.log)
 fibonacci(4)
 print(fibonacci.log)
